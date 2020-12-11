66594e601c4f24dc348251ff2e12ffa40e3a30c5
Fabien Luboz <lubozfabien@hotmail.com>
2020-12-11 21:28:22 +0100

    add log in spec

---
+++ test/log_inSpecs.js
@@ -0,0 +1,26 @@
+const chai = require('chai')
+const expect = require('chai').expect
+const mongoUnit = require('mongo-unit')
+let { app, server } = require('../index')
+
+describe('User Sign Up' , () => {
+    it('shoud register user', done => {
+        chai.request(app)
+        .post('/user/sign_up')
+        .send({
+            password: 'password',
+            email: "email@email.fr",
+            username: "username",
+            name: "name",
+            description: "description",
+        })
+        .end((err, res) => {
+            res.should.have.status(200)
+            expect(res.body.email).to.equal('email@email.fr')
+            expect(res.body.username).to.equal('username')
+            expect(res.body.description).to.equal('description')
+            expect(res.body.name).to.equal('name')
+            done()
+            })
+    })
+})
