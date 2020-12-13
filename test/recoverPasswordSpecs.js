68668d1466961a84c733a74b45f57886c6bba05b
Fabien Luboz <lubozfabien@hotmail.com>
2020-12-13 22:44:19 +0100

    add maigun stub

--- test/recoverPasswordSpecs.js
+++ test/recoverPasswordSpecs.js
@@ -5,9 +5,26 @@ let chaiHttp = require('chai-http')
 let { app, server } = require('../index')
 chai.use(chaiHttp);
 const mongoose = require('mongoose')
+const options = { hostname: 'api.mailgun.net',
+  port: 443,
+  protocol: 'https:',
+  path: '/v3/sandbox12345.mailgun.org/messages',
+  method: 'POST',
+  headers:
+   { 'Content-Type': 'application/x-www-form-urlencoded',
+     'Content-Length': 127 },
+  auth: 'api:key-0e8pwgtt5ylx0m94xwuzqys2-o0x4-77',
+  agent: false,
+  timeout: undefined }
+// const mailgun = require('mailgun-js')({ apiKey: process.env.MAILGUN_API_KEY, domain: process.env.MAILGUN_DOMAIN, testMode: true })
+// const nock = require('nock')
 const users = require('./fakeData/fakeUsers')
 
 const dbHandler = require('./db/db-handler')
+const sinon = require('sinon')
+const sandbox = sinon.createSandbox()
+
+
 
 
 describe('Recover user password', () => {
@@ -15,6 +32,15 @@ describe('Recover user password', () => {
         mongoose.disconnect()
         await dbHandler.connect()
         await dbHandler.insertData('users', [users.user1])
+       // const mailgunSendSpy = sandbox.stub().yields(null, { status: 200 })
+       // sandbox.stub(mailgun({ apiKey: 'foo', domain: 'bar', testMode: true }).Mailgun.prototype, 'messages') 
+       /*  .returns({ 
+            /* send: (data, cb) => {
+                console.log('data', data)
+                cb()
+            }  *
+            send: mailgunSendSpy
+        }) */
     }) 
     after('Process after test', async () => {
         await dbHandler.closeDatabase()
