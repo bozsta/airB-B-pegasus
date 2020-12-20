3f289d0c650f98ed9006013099fb728a9a6b57a0
Fabien Luboz <lubozfabien@hotmail.com>
2020-12-20 23:24:08 +0100

    TU: add get room by id

---
+++ test/getRoomById.js
@@ -0,0 +1,50 @@
+const chai = require('chai')
+const expect = chai.expect
+const should = chai.should();
+let chaiHttp = require('chai-http')
+let { app, server } = require('../index')
+chai.use(chaiHttp);
+const mongoose = require('mongoose')
+const users = require('./fakeData/fakeUsers')
+const rooms = require('./fakeData/FakeRooms')
+
+const dbHandler = require('./db/db-handler')
+
+describe('Get room by id', () => {
+    before('before hook', async () => {
+        mongoose.disconnect()
+        await dbHandler.connect()
+        await dbHandler.insertData('users', [users.user1, users.user2])
+        rooms.Room1.user = users.user1._id
+        rooms.Room2.user = users.user1._id
+        rooms.Room3.user = users.user2._id
+        await dbHandler.insertData('rooms', [rooms.Room1, rooms.Room2, rooms.Room3])
+    }) 
+    after('Process after test', async () => {
+        await dbHandler.closeDatabase()
+        server.close()
+    })
+    describe('Should success', () => {
+        it('get all rooms data', done => {
+            chai.request(app)
+            .get(`/room/222222222222222222222222`)
+            .end((req,res) => {
+                res.should.have.status(200)
+                expect(res.body._id).to.equal('222222222222222222222222')
+                expect(res.body.user.account.username).to.equal('username')
+                done()
+            })
+        })
+    })
+    describe('Should failled', () => {
+        it('wrong id', done => {
+            chai.request(app)
+            .get(`/room/444444444444444444444444`)
+            .end((req,res) => {
+                res.should.have.status(200)
+                expect(res.body).to.equal(null)
+                done()
+            })
+        })
+    })
+})
